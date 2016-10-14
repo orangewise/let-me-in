df3e36bb66069539a96a9f57dfbb2939cfe42c4d
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-10-14 08:48:48 +0200

    npm init

--- run.js
+++
@@ -1,9 +0,0 @@
-var join = require('path').join;
-var phantomjs = require('phantomjs-prebuilt');
-var url = process.argv[2];
-var program = phantomjs.exec(join(__dirname, 'codes.js'), url);
-program.stdout.pipe(process.stdout);
-program.stderr.pipe(process.stderr);
-program.on('exit', function (code) {
-  console.log('exit', code);
-});

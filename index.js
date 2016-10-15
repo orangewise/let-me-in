6092466c4d13759b14374b554b4b2c0aaa3fd7cb
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-10-15 11:10:55 +0200

    working on module

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var join = require('path').join;
 var phantomjs = require('phantomjs-prebuilt');
-var username = process.argv[2];
-var password = process.argv[3];
-var url = process.argv[4];
-var homePage = process.argv[5];
-var tokenPage = process.argv[6];
-var qty = process.argv[7] || 1;
 
-var program = phantomjs.exec(
-  join(__dirname, 'codes.js'),
-  username,
-  password,
-  url,
-  homePage,
-  tokenPage,
-  qty
-);
-program.stdout.pipe(process.stdout);
-program.stderr.pipe(process.stderr);
-program.on('exit', function (code) {
-  console.log('exit', code);
-});
+module.exports = letMeIn = {};
+
+letMeIn.getCodes = function (options) {
+
+  var program = phantomjs.exec(
+    join(__dirname, 'lib/codes.js'),
+    options.username,
+    options.password,
+    options.url,
+    options.homePage,
+    options.tokenPage,
+    options.qty
+  );
+  program.stdout.pipe(process.stdout);
+  program.stderr.pipe(process.stderr);
+  program.on('exit', function (code) {
+    console.log('exit', code);
+  });
+};

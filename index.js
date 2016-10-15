246d9761defee418bbee00e5724518d4d721de0a
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-10-15 13:25:25 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 var join = require('path').join;
 var phantomjs = require('phantomjs-prebuilt');
+var dummy = require('./lib/dummy');
 
 module.exports = letMeIn = {};
 
 letMeIn.getCodes = function (options) {
 
-  var program = phantomjs.exec(
-    join(__dirname, 'lib/codes.js'),
-    options.username,
-    options.password,
-    options.url,
-    options.homePage,
-    options.tokenPage,
-    options.qty
-  );
-  program.stdout.pipe(process.stdout);
-  program.stderr.pipe(process.stderr);
-  program.on('exit', function (code) {
-    console.log('exit', code);
-  });
+  // console.log('letMeIn.getCodes', options);
+  if (options.action === 'login') {
+    var program = phantomjs.exec(
+      join(__dirname, 'lib/codes.js'),
+      options.username,
+      options.password,
+      options.url,
+      options.homePage,
+      options.tokenPage,
+      options.qty
+    );
+    program.stdout.pipe(process.stdout);
+    program.stderr.pipe(process.stderr);
+    program.on('exit', function (code) {
+    // console.log('exit', code);
+    });
+    return program;
+  } else if (options.action === 'dummy') {
+    return dummy.codes(options);
+  }
 };
